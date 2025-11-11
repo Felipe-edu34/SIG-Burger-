cb3255eead2475cf95d71280d476a01742659fb6
felipe_eduardo <felipe.fernandes.710@ufrn.edu.br>
2025-11-11 17:17:56 -0300

    feat: adicionando o arquivo cardapio.c

---
+++ cardapio.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void menu_cardapio() {
+    
+    system("clear||cls");
+    printf("╔══════════════════════════════════════════════════╗\n");
+    printf("║                  MÓDULO CARDÁPIO                 ║\n");
+    printf("╠══════════════════════════════════════════════════╣\n");
+    printf("║                                                  ║\n");
+    printf("║ ► 1. Adicionar Item ao Cardápio                  ║\n");
+    printf("║ ► 2. Remover Item do Cardápio                    ║\n");
+    printf("║ ► 3. Atualizar Item do Cardápio                  ║\n");
+    printf("║ ► 4  pesquisar Item do cardapio                  ║\n");
+    printf("║ ► 5. Visualizar Cardápio                         ║\n");
+    printf("║ ► 0. Voltar ao Menu Principal                    ║\n");
+    printf("║                                                  ║\n");
+    printf("╚══════════════════════════════════════════════════╝\n"); 
+    printf("Escolha uma opção: ");
+
+}
