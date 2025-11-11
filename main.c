16ff73845b80a77a277a658192d3fd4c9410c8be
felipe_eduardo <felipe.fernandes.710@ufrn.edu.br>
2025-11-11 14:42:15 -0300

    feat: adicionando o arquivo main.c

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void menu_principal() {
+    
+    system("clear||cls");
+    printf("╔══════════════════════════════════════════════════╗\n");
+    printf("║ SIG-BURGER - SISTEMA DE GESTÃO DE HAMBURGUERIA   ║\n");
+    printf("╠══════════════════════════════════════════════════╣\n");
+    printf("║                                                  ║\n");
+    printf("║ ► 1. Módulo Clientes                             ║\n");
+    printf("║ ► 2. Módulo Pedidos                              ║\n");
+    printf("║ ► 3. Módulo Cardápio                             ║\n");
+    printf("║ ► 4. Módulo Estoque                              ║\n");
+    printf("║ ► 5. Módulo Financeiro                           ║\n");
+    printf("║ ► 6. Módulo Relatórios                           ║\n");
+    printf("║ ► 7. sobre o SIG-Burger                          ║\n");
+    printf("║ ► 0. Sair do Sistema                             ║\n");
+    printf("║                                                  ║\n");
+    printf("╚══════════════════════════════════════════════════╝\n");
+    printf("Escolha uma opção: ");
+
+}
