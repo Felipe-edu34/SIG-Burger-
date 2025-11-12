c2955cde36e73e4dcb5fa4ad0263e98088e73b80
elxsson <soullcakezz@gmail.com>
2025-11-12 10:45:01 -0300

    feat: adiciona header do modulo de cliente

---
+++ headers/cliente.h
@@ -0,0 +1,20 @@
+
+#ifndef CLIENTE_H
+#define CLIENTE_H
+
+typedef struct {
+    char nome[100];
+    char telefone[15];
+    char endereco[200];
+    int status; // 1 = ativo, 0 = inativo
+} Cliente;
+
+
+void cliente(void);
+void menu_clientes(void);
+void cadastrar_cliente(void);
+void listar_clientes(void);
+void editar_cliente(void);
+void excluir_cliente(void);
+
+#endif
