8d813909ce66c0ca14ca960ce60f6191a6280d1e
felipe_eduardo <felipe.fernandes.710@ufrn.edu.br>
2025-11-16 11:17:00 -0300

    feat: adicionando a navegação do modulo relatorio

--- modulos/relatorio.c
+++ modulos/relatorio.c
@@ -24,3 +24,46 @@ void menu_relatorio(){
         printf("╚══════════════════════════════════════════════════╝\n");
         printf("Escolha uma opção: ");
 }
+
+
+
+void relatorio() {
+    int opcao;
+
+    do {
+        menu_relatorio();
+        scanf("%d", &opcao);
+        limparBuffer();
+
+        switch (opcao) {
+            case 1:
+                printf("Relatório de Cardápio selecionado.\n");
+                // Chamar função do módulo Cardápio
+                break;
+            case 2:
+                printf("Relatório de Estoque selecionado.\n");
+                // Chamar função do módulo Estoque
+                break;
+            case 3:
+                printf("Relatório de Clientes selecionado.\n");
+                // Chamar função do módulo Clientes
+                break;
+            case 4:
+                printf("Relatório de Pedidos selecionado.\n");
+                // Chamar função do módulo Pedidos
+                break;
+            case 5:
+                printf("Relatório Financeiro selecionado.\n");
+                // Chamar função do módulo Financeiro
+                break;
+            case 0:
+                printf("\nVoltando ao Menu Principal...\n");
+                pausar();
+                break;
+            default:
+                printf("\nOpção inválida! Tente novamente.\n");
+                pausar();
+                break;
+        }
+    } while (opcao != 0);
+}
