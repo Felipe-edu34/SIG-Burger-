2503964d8189a1f85ea3092a99ea148e975dad89
felipe_eduardo <felipe.fernandes.710@ufrn.edu.br>
2025-11-17 13:49:09 -0300

    feat: adicionando o header file de ler preço

---
+++ headers/leitura.h
@@ -0,0 +1,12 @@
+#ifndef LEITURA_H  
+#define LEITURA_H
+
+float ler_preco(float *preco);
+void ler_nome_produto(char *nome);
+void ler_categoria_estoque(char *categoria);
+void ler_nome_item_cardapio(char *nome);
+void ler_categoria_cardapio(char *categoria);
+void ler_descricao_item_cardapio(char *descricao);
+void ler_validade(char *validade);
+
+#endif // LEITURA_H
