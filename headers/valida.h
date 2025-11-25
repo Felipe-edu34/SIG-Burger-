723dad44596b6a675eb5b816ec5370c5630a65fe
felipe_eduardo <felipe.fernandes.710@ufrn.edu.br>
2025-11-24 21:41:35 -0300

    feat: adicionando a função de validar_cpf

--- headers/valida.h
+++ headers/valida.h
@@ -10,5 +10,6 @@ int valida_quantidade(int* quantidade);
 int valida_telefone(const char* telefone);
 int valida_endereco(const char* endereco);
 int validar_nome_produto(const char *nome);
+int valida_cpf(const char* cpf);
 
 #endif // VALIDA_H
