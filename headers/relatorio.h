dfef2b368d2a2933512a42781e7ee17cf6357042
felipe_eduardo <felipe.fernandes.710@ufrn.edu.br>
2025-11-27 18:06:01 -0300

    style: colocando comentarios para melhor entendimento das funções

--- headers/relatorio.h
+++ headers/relatorio.h
@@ -7,13 +7,23 @@
 
 void menu_relatorio();
 void relatorio();
-void relatorio_estoque();
+
+//////////////////////////////RELATÓRIOS DE CARDÁPIO////////////////////////////////
+
 void relatorio_cardapio();
+void listar_itens_por_preco();
 void exibindo_cardapio_por_categoria();
 void relatorio_itens_por_status(int status, const char* titulo);
 void relatorio_cardapio_itens_disponiveis();
 void relatorio_cardapio_itens_indisponiveis();
 void procurar_item_por_categoria();
+void liberar_lista_cardapio(NodeItem *lista);
+void exibir_lista_itens(NodeItem *lista);
+NodeItem* montar_lista_itens_ordenados_preco();
+
+///////////////////////////////RELATÓRIOS DE ESTOQUE////////////////////////////////
+
+void relatorio_estoque();
 void exibir_itens_com_baixa_quantidade();
 void exibir_itens_indisponiveis_estoque();
 void exibir_itens_por_nome();
@@ -24,12 +34,15 @@ void ordenar_por_quantidade(Nodeproduto *lista);
 Nodeproduto* montar_lista_estoque();
 void listar_todo_estoque();
 void exibir_lista_estoque(Nodeproduto *lista);
-NodeItem* montar_lista_itens_ordenados_preco();
-void liberar_lista_cardapio(NodeItem *lista);
 void listar_itens_por_preco();
-void exibir_lista_itens(NodeItem *lista);
+
+//////////////////////////////RELATÓRIOS DE CLIENTES////////////////////////////////
+
 void relatorio_clientes();
 void relatorio_clientes_com_ultimo_pedido();
+
+//////////////////////////////RELATÓRIOS DE PEDIDOS////////////////////////////////
+
 void exibir_pedidos_por_status();
 void relatorio_delivery_vs_local();
 void relatorio_itens_mais_pedidos();
